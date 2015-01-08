c0b3bebdd9e8ecbf0139b4af300878d561993891
martin coulon <martinusbe@gmail.com>
2015-01-08 06:47:51 +0100

    Enable changelog

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_GCC_VERSION_ARM := 4.9-sm
 SYMMETRY := true
 GRAPHITE_OPTS := true
 STRICT_ALIASING := true
+VALIDUS_CHANGELOG := true
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
