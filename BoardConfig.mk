aeef2f7ae3d44e1d84326358f2558ba5892661b8
martin coulon <martinusbe@gmail.com>
2015-02-02 17:51:43 +0100

    Boardconfig: Disable changelog

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_GCC_VERSION_ARM := 4.9-sm
 SYMMETRY := true
 GRAPHITE_OPTS := true
 STRICT_ALIASING := true
-VALIDUS_CHANGELOG := true
+#VALIDUS_CHANGELOG := true
 NO_BLOCK_OTA := false
 
 # Include an expanded selection of fonts
